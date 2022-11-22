340ad769eb76d5d8d2234793a35e67527b91a77a
github akito-10 <akt.fukuda@gmail.com>
2022-11-23 02:46:59 +0900

    ✨ setup msw on react18

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { worker } from './mocks/browser';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  /**
+   * - set quiet to false if you want to see API responses in the console
+   * - onUnhandledRequest will just hide the warnings about APIs not being intercepted
+   */
+  worker
+    .start({
+      quiet: true,
+      onUnhandledRequest: 'bypass',
+      serviceWorker: {
+        url: '/mockServiceWorker.js',
+      },
+    })
+    .then(() => {
+      return root.render(<App />);
+    });
+} else {
+  throw new Error('No container with the name of root found');
+}
